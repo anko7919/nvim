b81869256a77b26b40af15bd20488f91117396ef
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-09-07 22:34:45 +0900

    telescope.nvim追加

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -12,8 +12,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- color scheme
+-- PLUGINS
 require("lazy").setup({
+    -- color scheme
     {
         "cocopon/iceberg.vim", 
         lazy = false, 
@@ -22,14 +23,29 @@ require("lazy").setup({
             vim.cmd("colorscheme iceberg")
         end, 
     }, 
-})
 
--- 括弧補間
-require("lazy").setup({
+    -- 括弧補間
     {
         "windwp/nvim-autopairs", 
         event = "InsertEnter", 
         config = true, 
+    }, 
+
+    -- 探索
+    {
+        "nvim-telescope/telescope.nvim", 
+        dependencies = { "nvim-lua/plenary.nvim" }, 
+        lazy = false, 
+        config = function()
+            local builtin = require("telescope.builtin")
+            local opts = { noremap = true, silent = true }
+
+            -- キーマップ設定
+            vim.keymap.set("n", "<leader>ff", builtin.fild_files, opts) -- ファイル検索
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, opts)  -- テキスト検索
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, opts)    -- バッファ検索
+            vim.keymap.set("n", "<leader>fr", builtin.oldfiles, opts)   -- 最近開いたファイル
+        end, 
     }, 
 })
 
