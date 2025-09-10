84acbaeff7c1d6097750ba2e26d8f7877d66e85f
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-09-10 22:58:37 +0900

    stevearc/oil.nvimを追加

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -81,5 +81,17 @@ require("lazy").setup({
             })
         end, 
     }, 
+
+    {
+        "stevearc/oil.nvim", 
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {}, 
+        dependencies = { { "echasnovski/mini.icons" , opts = {} } }, 
+        lazy = false, 
+	config = function()
+	    require("oil").setup()
+	end, 
+    }, 
 })
 
