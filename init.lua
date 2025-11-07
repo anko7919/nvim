9b5205288cb58b7f0489aeb0b9e29dadc7c6e98d
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-11-07 17:28:10 +0900

    ディレクトリ構成を変更

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 -- ファイル分割読み込み
-require("user.settings")
-require("user.keymaps")
-require("user.plugins.others")
-require("user.indents")
-require("user.plugins.mason")
-require("user.plugins.lsp")
+require("settings")
+require("keymaps")
+require("plugins.others")
+require("indents")
+require("plugins.mason")
+require("plugins.lsp")
 
