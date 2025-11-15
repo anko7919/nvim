e72931acef676ab55b9a8834ea312f73190ff778
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-11-15 13:48:06 +0900

    vimtexの設定を追加

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -102,7 +102,11 @@ return {
         lazy = false, 
         ft = "tex", 
         init = function()
-
+            -- vim.g.vimtex_view_method = "/mnt/c/Users/haupt/AppData/Local/SumatraPDF/SumatraPDF.exe" 
+            vim.g.vimtex_view_method = "general"
+            vim.g.vimtex_fold_enabled = false
+            vim.g.vimtex_format_enabled = true
+            vim.g.vimtex_view_general_viewer = "/mnt/c/Users/haupt/AppData/Local/SumatraPDF/SumatraPDF.exe"
         end, 
     }, 
 
