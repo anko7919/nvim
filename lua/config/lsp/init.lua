c914ce1d8f42cd4843011beaab8fd97ab2195f28
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-11-22 15:38:29 +0900

    LSP設定追加

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,8 +1,13 @@
+-- 使用するLSPサーバー
 local lsp_servers = {
     "clangd", 
     "rust_analyzer", 
+    "cmake", 
+    "julials", 
+    "pyright", 
 }
 
+-- lsp_serversのそれぞれを読み込み
 for _, server in ipairs(lsp_servers) do
     vim.lsp.enable(server)
     vim.lsp.config[server] = require("config.lsp." .. server)
