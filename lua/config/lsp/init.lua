ffac556de916cb715b6eb94cba2f5090e9fcd085
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-11-23 10:48:47 +0900

    lspのinitの書き方変更

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -10,6 +10,6 @@ local lsp_servers = {
 -- lsp_serversのそれぞれを読み込み
 for _, server in ipairs(lsp_servers) do
     vim.lsp.enable(server)
-    vim.lsp.config[server] = require("config.lsp." .. server)
+    vim.lsp.config(server, require("config.lsp." .. server))
 end
 
