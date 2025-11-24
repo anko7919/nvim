690e68dcf960fa4ca57a9c8725e9064f05dab7f3
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-11-24 13:29:50 +0900

    accelerated-jkを追加

---
+++ lua/plugins/accelerated_jk.lua
@@ -0,0 +1,12 @@
+return {
+    "rhysd/accelerated-jk", 
+    lazy = false, 
+    keys = {
+        { "j", "<plug>(accelerated_jk_gj_position)", mode = "n" }, 
+        { "k", "<plug>(accelerated_jk_gk_position)", mode = "n" }, 
+    }, 
+    config = function() 
+
+    end,
+}
+
