7a9fbe8286d0d6c883e1b472838495679375bec0
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-11-24 21:13:51 +0900

    snacks.nvimによるdashboard設定

--- lua/plugins/snacks.nvim.lua
+++ lua/plugins/snacks.nvim.lua
@@ -3,6 +3,24 @@ return {
         "folke/snacks.nvim", 
         priority = 1000, 
         lazy = false, 
+        opts = {
+            dashboard = {
+                row = 10, 
+                sections = {
+                    {
+                        section = "terminal", 
+                        cmd = "chafa ~/.config/nvim/goch.png --format symbols --symbols=block --size=60x17 --stretch; sleep .1", 
+                        height = 17, 
+                        padding = 1, 
+                    }, 
+                    {
+                        pane = 2, 
+                        { section = "keys", gap = 1, padding = 1 }, 
+                        { section = "startup" }, 
+                    }
+                }
+            }
+        }, 
         init = function()
             require("snacks").setup({})
         end, 
