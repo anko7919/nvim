89b23515875b925b3795ac39e6b1e6ddb289f37d
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2025-12-03 15:01:01 +0900

    change configure of nvim-hlslens: because its apparence was not good

--- lua/plugins/nvim-hlslens.lua
+++ lua/plugins/nvim-hlslens.lua
@@ -1,6 +1,26 @@
 return {
-    "kevinhwang91/nvim-hlslens", 
-    opts = {}, 
-    event = "BufReadPost", 
+    "kevinhwang91/nvim-hlslens",
+    event = "BufReadPost",
+    opts = {},
+    keys = {
+        {
+            "n",
+            function()
+                vim.cmd("normal! " .. vim.v.count1 .. "n")
+                require("hlslens").start()
+            end,
+            mode = "n",
+            desc = "Next search result + hlslens",
+        },
+        {
+            "N",
+            function ()
+                vim.cmd("normal! " .. vim.v.count1 .. "n")
+                require("hlslens").start()
+            end,
+            mode = "n",
+            desc = "Previous search result + hlslens",
+        }
+    },
 }
 
