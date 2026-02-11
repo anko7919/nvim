f5e1636f28dfa81cb56b3a45ad822df3fa85b9c5
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-02-11 23:16:24 +0900

    行頭・行末へ移動するキーマップを追加: よく使う＆デフォルトは使いづらいため

--- lua/plugins/which-key.nvim.lua
+++ lua/plugins/which-key.nvim.lua
@@ -6,30 +6,39 @@ return {
             local wk = require("which-key")
             wk.setup()
             wk.add({
+                -- Telescope
                 { "<leader>f", group = "Telescope File" }, 
                 { "<leader>ff", desc = "Find File", mode = "n" }, 
                 { "<leader>fg", desc = "Live Grep", mode = "n" }, 
                 { "<leader>fb", desc = "Buffer", mode = "n" }, 
                 { "<leader>fr", desc = "Recent File", mode = "n" }, 
 
+                -- Terminal
                 { "<leader>t", group = "Terminal" }, 
                 { "<leader>tr", desc = "Right", mode = "n" }, 
                 { "<leader>tb", desc = "Below", mode = "n" }, 
 
+                -- VimTex Delete
                 { "ds", group = "VimTex Delete" }, 
                 { "dse", desc = "Delete Environment", mode = "n" }, 
                 { "dsc", desc = "Delete Command", mode = "n" }, 
                 { "dsd", desc = "Delete Delimiter", mode = "n" }, 
                 { "ds$", desc = "Delete Math", mode = "n" }, 
 
+                -- VimTex Change
                 { "cs", group = "VimTex Change"}, 
                 { "csc", desc = "Change Environment", mode = "n" }, 
                 { "cse", desc = "Change Entironment", mode = "n" }, 
                 { "csd", desc = "Change Delimiter", mode = "n" }, 
                 { "cs$", desc = "Change Math", mode = "n" }, 
 
+                -- Oil
                 { "<leader>spo", desc = "Split with Oil", mode = "n" }, 
                 { "<leader>vso", desc = "Split Vertically with Oil", mode = "n" }, 
+
+                -- Head/Tail of Line
+                { "<space>h", desc = "Go to Head of Line", mode = "n" },
+                { "<space>l", desc = "Go to Tile of Line", mode = "n" },
             })
         end, 
     }
