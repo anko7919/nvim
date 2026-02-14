38c65666485decf315f5c7ed0bd5d6186fbfde5b
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-02-14 23:07:19 +0900

    ダッシュボードを更新

--- lua/plugins/snacks.nvim.lua
+++ lua/plugins/snacks.nvim.lua
@@ -1,3 +1,10 @@
+local math_quotes = {
+    "La beauté d’une théorie ne réside pas dans son utilité, mais\ndans sa cohérence.",
+}
+
+-- Pick a random quote
+local quote = math_quotes[math.random(#math_quotes)]
+
 return {
     {
         "folke/snacks.nvim", 
@@ -5,44 +12,63 @@ return {
         lazy = false, 
         opts = {
             dashboard = {
-                header = { "NeoVim" }, 
                 padding_top = 5, 
                 padding_bottom = 5, 
                 row = nil, 
                 col = nil, 
-                pane_gap = 4, 
+                pane_gap = 3, 
+                preset = {
+                    ---@type snacks.dashboard.Item[]
+                    keys = {
+                        { icon = "* ", key = "f", desc = "Find File", action = ":Telescope find_files" },
+                        { icon = "* ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                        { icon = "* ", key = "g", desc = "Find Text", action = ":Telescope live_grep" },
+                        { icon = "* ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                        { icon = "* ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                        { icon = "* ", key = "s", desc = "Edit Snippets", action = ":lua require('luasnip.loaders').edit_snippet_files()" },
+                        { icon = "* ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                        { icon = "* ", key = "q", desc = "Quit", action = ":qa" },
+                    },
+                },
                 sections = {
                     {
                         {
-                            section = "header", 
-                            padding = 2, 
-                        }, 
+                            section = "header",
+                            padding = 1,
+                        },
                         {
                             section = "terminal", 
                             cmd = "chafa ~/.config/nvim/goch.png --format symbols --symbols=block --size=60x17 --stretch; sleep .1", 
                             height = 17, 
-                            padding = 1, 
+                            padding = 2, 
                         }, 
                         {
                             section = "startup", 
-                            padding = 1, 
+                            padding = 1,
                         }, 
                     }, 
                     {
                         pane = 2, 
+                        {
+                            section = "terminal",
+                            cmd = "cmatrix -rb -u15",
+                            height = 6,
+                            padding = 1,
+                        },
                         { 
                             section = "keys", 
+                            title = quote,
                             gap = 1, 
-                            padding = 1, 
-                            indent = 2, 
+                            padding = 2, 
+                            indent = 1, 
+                            item = keys,
                         }, 
-                        { 
-                            section = "terminal",  
-                            title = "Git Status", 
-                            cmd = "git status --short --branch --renames", 
-                            indent = 2, 
-                            padding = 1, 
-                        }, 
+                        {
+                            section = "terminal",
+                            cmd = "nvim --version | head -n1",
+                            height = 1,
+                            indent = 23,
+                        },
                     }
                 }
             }
