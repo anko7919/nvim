4516719cb8f05565e6a78e4f78b0629d94169c16
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-02-17 16:16:20 +0900

    vim-asterisk.luaを更新

--- lua/plugins/vim-asterisk.lua
+++ lua/plugins/vim-asterisk.lua
@@ -2,7 +2,13 @@ return {
     "haya14busa/vim-asterisk",
     event = "BufReadPre",
     keys = {
-        { "*", "<Plug>(asterisk-*)", mode = "n", },
+        { "*", "<Plug>(asterisk-z*)", mode = "n", },
+        { "#", "<Plug>(asterisk-z#)", mode = "n", },
+        { "g*", "<Plug>(asterisk-gz*)", mode = "n", },
+        { "g#", "<Plug>(asterisk-gz#)", mode = "n", },
     },
+    config = function()
+        vim.g["asterisk#keeppos"] = 1
+    end
 }
 
