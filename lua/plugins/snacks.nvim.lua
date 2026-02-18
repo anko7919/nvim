ccb84edb7151fab7a4b0e31bd7ea8b5fe42aafe5
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-02-18 18:07:20 +0900

    snacks.nvimのダッシュボードを調整: フォントサイズの変更により動かなくなったため

--- lua/plugins/snacks.nvim.lua
+++ lua/plugins/snacks.nvim.lua
@@ -10,6 +10,7 @@ return {
                 row = nil, 
                 col = nil, 
                 pane_gap = 3, 
+                width = 48,
                 preset = {
                     ---@type snacks.dashboard.Item[]
                     keys = {
@@ -26,40 +27,33 @@ return {
                 sections = {
                     {
                         {
-                            section = "header",
-                            padding = 1,
-                        },
-                        {
                             section = "terminal", 
-                            cmd = "chafa ~/.config/nvim/goch.png --format symbols --symbols=block --size=60x17 --stretch; sleep .1", 
-                            height = 17, 
-                            padding = 2, 
+                            cmd = "chafa ~/.config/nvim/goch.png --format symbols --symbols=block --size=47x14 --stretch; sleep .1", 
+                            height = 14, 
+                            padding = 1, 
                         }, 
                         {
                             section = "startup", 
-                            padding = 1,
                         }, 
                     }, 
                     {
                         pane = 2, 
                         {
                             section = "terminal",
-                            cmd = "cmatrix -rb -u15",
-                            height = 6,
+                            cmd = "cmatrix -rb -u18",
+                            height = 5,
                             padding = 1,
                         },
                         { 
                             section = "keys", 
-                            gap = 1, 
-                            padding = 4, 
-                            indent = 1, 
+                            padding = 1, 
                             item = keys,
                         }, 
                         {
                             section = "terminal",
                             cmd = "nvim --version | head -n1",
                             height = 1,
-                            indent = 23,
+                            indent = 17,
                         },
                     }
                 }
