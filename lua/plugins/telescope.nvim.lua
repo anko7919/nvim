e8e5344321bf815f4b6ef94974b9650753052708
Anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-03-09 15:32:02 +0900

    telescope.nvim.luaを更新

--- lua/plugins/telescope.nvim.lua
+++ lua/plugins/telescope.nvim.lua
@@ -7,18 +7,34 @@ return {
         }, 
         lazy = false, 
         keys = {
-            { "<leader>ff", "<cmd>Telescope find_files<cr>", mode = { "n" }, desc = "Find files" }, 
-            { "<leader>fg", "<cmd>Telescope live_grep<cr>", mode = { "n" }, desc = "Telescope live grep" }, 
-            { "<leader>fb", "<cmd>Telescope buffers<cr>", mode = { "n" }, desc = "Telescope buffers" }, 
-            { "<leader>fh", "<cmd>Telescope help_tags<cr>", mode = { "n" }, desc = "Telescope help tags" }, 
+            -- 検索
+            { "<leader>ff", require("telescope.builtin").find_files, mode = { "n" }, desc = "Find files" }, 
+            { "<leader>fg", require("telescope.builtin").live_grep, mode = { "n" }, desc = "Telescope live grep" }, 
+            { "<leader>fG", require("telescope.builtin").grep_string, mode = { "n" }, desc = "Search for Cursor string"},
+            { "<leader>fr", require("telescope.builtin").old_files, mode = { "n" }, desc = "Recent files" },
 
-            { "<leader>sr", "<cmd>Telescope lsp_references<cr>", mode = { "n" }, desc = "Lists LSP references", },
-            { "<leader>sD", "<cmd>Telescope diagnostics<cr>", mode = { "n" }, desc = "Lists Diagnostics", },
-            { "<leader>si", "<cmd>Telescope lsp_implementations<cr>", mode = { "n" }, desc = "Goto the implementation", },
-            { "<leader>sd", "<cmd>Telescope lsp_definitions<cr>", mode = { "n" }, desc = "Goto the definition", },
+            -- LSP
+            { "<leader>sr", require("telescope.builtin").lsp_references, mode = { "n" }, desc = "Lists LSP references", },
+            { "<leader>sD", require("telescope.builtin").diagnostics, mode = { "n" }, desc = "Lists Diagnostics", },
+            { "<leader>si", require("telescope.builtin").lsp_implementations, mode = { "n" }, desc = "Goto the implementation", },
+            { "<leader>sd", require("telescope.builtin").lsp_definitions, mode = { "n" }, desc = "Goto the definition", },
+            { "<leader>st", require("telescope.builtin").lsp_type_definitions, mode = { "n" }, desc = "Go to Type definition", },
             
-            { "<leader>bl", "<cmd>Telescope buffers<cr>", mode = { "n" }, desc = "Lists open buffers", },
-        }
+            -- バッファ
+            { "<leader>bl", require("telescope.builtin").buffers, mode = { "n" }, desc = "Lists open buffers", },
+
+            -- Git
+            { "<leader>gB", require("telescope.builtin").git_branches, mode = { "n" }, desc = "Lists all branches", },
+            { "<leader>gs", require("telescope.builtin").git_status, mode = { "n" }, desc = "Git Status" },
+        },
+        opts = {
+            defaults = {
+                layout_strategy = "vertical",
+                layout_config = {
+                    vertical = { width = 80, },
+                },
+            },
+        },
     }
 }
 
