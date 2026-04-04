607d4c7c989a903b1b4173e52dc69afd05a2c81f
anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-04-04 23:32:30 +0900

    rebase

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,38 +1,39 @@
 return {
-    "neovim/nvim-lspconfig", 
-    event = { "BufReadPre" }, 
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre" },
     dependencies = {
-        "mason.nvim", 
-        { "mason-org/mason-lspconfig.nvim", config = function() end }, 
+        "mason.nvim",
+        { "mason-org/mason-lspconfig.nvim", config = function() end },
         "LazyVim",
-    }, 
+    },
     opts = function()
         local ret = {
             diagnostics = {
-                underline = true, 
-                update_in_insert = false, 
-            }, 
-            severity_sort = true, 
+                underline = true,
+                update_in_insert = false,
+            },
+            severity_sort = true,
             signs = {
                 text = {
-                    [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error, 
-                    [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn, 
-                    [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint, 
-                    [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info, 
-                }, 
-            }, 
+                    [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+                    [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+                    [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+                    [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+                },
+            },
             inlay_hints = {
-                enabled = true, 
-            }, 
+                enabled = true,
+            },
 
             servers = {
-                ["clangd"] = require("plugins.lsp.clangd"), 
-                ["cmake"] = require("plugins.lsp.cmake"), 
-                ["julials"] = require("plugins.lsp.julials"), 
-                ["pyright"] = require("plugins.lsp.pyright"), 
-                ["rust_analyzer"] = require("plugins.lsp.rust_analyzer"), 
-                ["texlab"] = require("plugins.lsp.texlab"), 
-            }, 
+                ["clangd"] = require("plugins.lsp.clangd"),
+                ["cmake"] = require("plugins.lsp.cmake"),
+                ["julials"] = require("plugins.lsp.julials"),
+                ["pyright"] = require("plugins.lsp.pyright"),
+                ["rust_analyzer"] = require("plugins.lsp.rust_analyzer"),
+                ["texlab"] = require("plugins.lsp.texlab"),
+                ["lua_ls"] = require("plugins.lsp.lua_ls"),
+            },
 
             keys = {
                 { "gd", vim.lsp.buf.definition, desc = "Goto Definition", has = "definition" },
@@ -41,16 +42,16 @@ return {
                 { "<c-k>", function() return vim.lsp.buf.signature_help() end, mode = "i", desc = "Signature Help", has = "signatureHelp" },
                 { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "x" }, has = "codeAction" },
                 { "<leader>cr", vim.lsp.buf.rename, desc = "Rename", has = "rename" },
-            },       
+            },
         }
         return ret
-    end, 
+    end,
 
     config = function(_, opts)
-        for server, server_opts in pairs(opts.servers) do 
+        for server, server_opts in pairs(opts.servers) do
             vim.lsp.config(server, server_opts)
             vim.lsp.enable(server)
         end
-    end, 
+    end,
 }
 
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,19 +1,20 @@
 return {
-    "mason-org/mason-lspconfig.nvim", 
+    "mason-org/mason-lspconfig.nvim",
     lazy = false,
-    opts = { 
+    opts = {
         ensure_installed = {
-            "rust_analyzer", 
-            "clangd", 
-            "pyright", 
-            "cmake", 
-            "julials", 
-            "texlab", 
+            "rust_analyzer",
+            "clangd",
+            "pyright",
+            "cmake",
+            "julials",
+            "texlab",
+            "lua_ls",
         }
-    }, 
+    },
     dependencies = {
-        { "mason-org/mason.nvim", opts = {} }, 
-        "neovim/nvim-lspconfig", 
-    }, 
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
 }
 
