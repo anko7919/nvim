fedca541036dc182900114e291cabac55726f92e
anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-04-04 10:45:43 +0900

    skkeleton調整

--- lua/plugins/skkeleton.lua
+++ lua/plugins/skkeleton.lua
@@ -1,23 +1,35 @@
 return {
     "vim-skk/skkeleton",
+    event = "InsertEnter",
     dependencies = {
         "vim-denops/denops.vim",
     },
     keys = {
-        { "<C-j>", "<Plug>(skkeleton-enable)", mode = { "i", "c", "t", }, desc = "skkeleton enable" },
+        { "<C-j>", "<Plug>(skkeleton-toggle)", mode = { "i", "c", "t", }, desc = "skkeleton toggle" },
     },
     config = function()
         local function skkeleton_init()
             vim.fn["skkeleton#config"]({
-                globalDictionaries = { "~/.skk/SKK-JISYO.L" }, 
+                globalDictionaries = { "~/.skk/SKK-JISYO.L", "~/.skk/SKK-JISYO.math" }, 
                 eggLikeNewline = true,
-                userDictionary = "~/.skk/SKK-JISYO.math",
+                userDictionary = "~/.skk/SKK-JISYO.user",
                 showCandidatesCount = 1,
                 registerConvertResult = true,
             })
             vim.fn["skkeleton#register_kanatable"]("rom", {
                 [","] = { "，", "" },
                 ["."] = { "．", "" },
+            })
+            vim.fn["skkeleton#register_kanatable"]("rom", {
+                l = false,
+                la = { "ぁ", "" },
+                li = { "ぃ", "" },
+                lu = { "ぅ", "" },
+                le = { "ぇ", "" },
+                lo = { "ぉ", "" },
+                lya = { "ゃ", "" },
+                lyu = { "ゅ", "" },
+                lyo = { "ょ", "" },
             })
         end
 
