eaa09e9a5f273562b4df84d116ac98304051dc1b
anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-04-05 12:33:53 +0900

    lazydevをnvim-cmpと連携させた

--- lua/plugins/lazydev.nvim.lua
+++ lua/plugins/lazydev.nvim.lua
@@ -1,11 +1,23 @@
 return {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    opts = {
-        library = {
-            "lazy.nvim",
-            "LazyVim",
-        }
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                "lazy.nvim",
+                "LazyVim",
+            }
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        opts = function(_, opts)
+            opts.sources = opts.sources or {}
+            table.insert(opts.sources, {
+                name = "lazydev",
+                group_index = 0, -- set group index to 0 to skip loading LuaLS compeltions
+            })
+        end,
     },
 }
 
