17dd6ba33d6589fca269790d39ec8a525e5235c2
anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-04-05 12:34:11 +0900

    which-key.nvimを修正

--- lua/plugins/which-key.nvim.lua
+++ lua/plugins/which-key.nvim.lua
@@ -1,47 +1,45 @@
 return {
     {
-        "folke/which-key.nvim", 
-        event = "InsertEnter", 
+        "folke/which-key.nvim",
+        event = "VeryLazy",
         config = function()
             local wk = require("which-key")
             wk.setup()
             wk.add({
                 -- Telescope
-                { "<leader>f", group = "Telescope File" }, 
-                { "<leader>ff", desc = "Find File", mode = "n" }, 
-                { "<leader>fg", desc = "Live Grep", mode = "n" }, 
-                { "<leader>fb", desc = "Buffer", mode = "n" }, 
-                { "<leader>fr", desc = "Recent File", mode = "n" }, 
-
-                { "<leader>l", group = "Telescope LSP" },
+                { "<leader>f", group = "Telescope File" },
+                { "<leader>ff", desc = "Find File", mode = "n" },
+                { "<leader>fg", desc = "Live Grep", mode = "n" },
+                { "<leader>fb", desc = "Buffer", mode = "n" },
+                { "<leader>fr", desc = "Recent File", mode = "n" },
 
                 -- Terminal
-                { "<leader>t", group = "Terminal" }, 
-                { "<leader>tr", desc = "Right", mode = "n" }, 
-                { "<leader>tb", desc = "Below", mode = "n" }, 
+                { "<leader>t", group = "Terminal" },
+                { "<leader>tr", desc = "Right", mode = "n" },
+                { "<leader>tb", desc = "Below", mode = "n" },
 
-                -- VimTex Delete
-                { "ds", group = "VimTex Delete" }, 
-                { "dse", desc = "Delete Environment", mode = "n" }, 
-                { "dsc", desc = "Delete Command", mode = "n" }, 
-                { "dsd", desc = "Delete Delimiter", mode = "n" }, 
-                { "ds$", desc = "Delete Math", mode = "n" }, 
+                -- VimTeX Delete
+                { "ds", group = "VimTex Delete" },
+                { "dse", desc = "Delete Environment", mode = "n" },
+                { "dsc", desc = "Delete Command", mode = "n" },
+                { "dsd", desc = "Delete Delimiter", mode = "n" },
+                { "ds$", desc = "Delete Math", mode = "n" },
 
-                -- VimTex Change
-                { "cs", group = "VimTex Change" }, 
-                { "csc", desc = "Change Command", mode = "n" }, 
-                { "cse", desc = "Change Entironment", mode = "n" }, 
-                { "csd", desc = "Change Delimiter", mode = "n" }, 
-                { "cs$", desc = "Change Math", mode = "n" }, 
+                -- VimTeX Change
+                { "cs", group = "VimTex Change" },
+                { "csc", desc = "Change Command", mode = "n" },
+                { "cse", desc = "Change Entironment", mode = "n" },
+                { "csd", desc = "Change Delimiter", mode = "n" },
+                { "cs$", desc = "Change Math", mode = "n" },
 
                 -- Oil
-                { "<leader>spo", desc = "Split with Oil", mode = "n" }, 
-                { "<leader>vso", desc = "Split Vertically with Oil", mode = "n" }, 
+                { "<leader>spo", desc = "Split with Oil", mode = "n" },
+                { "<leader>vso", desc = "Split Vertically with Oil", mode = "n" },
 
                 -- gitsigns
                 { "<leader>g", group = "Git", mode = "n" },
             })
-        end, 
+        end,
     }
 }
 
