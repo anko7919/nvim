427874988a605000713760f23462f92060363394
anko7919 <hauptidealsatz.anko7919@gmail.com>
2026-06-13 22:58:39 +0900

    diffview追加したり色々

--- lua/plugins/diffview.nvim.lua
+++ lua/plugins/diffview.nvim.lua
@@ -1,5 +1,12 @@
 return {
     "sindrets/diffview.nvim",
-    enabled = false,
+    keys = {
+        { "<leader>do", "<cmd>DiffviewOpen<cr>", mode = "n", desc = "open a new diffview" },
+        { "<leader>df", "<cmd>DiffviewFileHistory<cr>", mode = "n", desc = "open a new file history view" },
+        { "<leader>dq", "<cmd>DiffviewClose<cr>", mode = "n", desc = "close the current diffview" },
+    },
+    opts = {
+        use_icons = false,
+    },
 }
 
--- lua/plugins/oil.nvim.lua
+++ lua/plugins/oil.nvim.lua
@@ -1,31 +1,31 @@
 return {
     {
-        "stevearc/oil.nvim", 
+        "stevearc/oil.nvim",
         ---@module "oil"
         ---@type oil.SetupOpts
-        opts = {}, 
-        dependencies = { { "nvim-mini/mini.icons", opts = {} } }, 
+        opts = {},
+        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
         cmd = { "Oil" },
         keys = {
-            { "-", "<cmd>Oil --float<cr>", mode = "n", desc = "Open Oil.nvim" }, 
-        }, 
+            { "-", "<cmd>Oil --float<cr>", mode = "n", desc = "Open Oil.nvim" },
+        },
         config = function()
             local oil = require("oil")
             oil.setup({
                 delete_on_trash = true,
                 default_file_explorer = true,
                 watch_for_changes = true,
-                columns = {
-                    --"icon", 
-                    --"permissions", 
-                    "size", 
-                    "mtime", 
-                },
+                columns = {},
                 float = {
                     padding = 2,
                     max_width = 0.85,
                     max_height = 0.85,
                     border = "rounded",
+                },
+                lsp_file_methods = {
+                    enabled = true,
+                    timeout_ms = 1000,
+                    autosave_changes = false,
                 },
                 keymaps = {
                     ["<CR>"] = "actions.select",
@@ -33,11 +33,12 @@ return {
                     ["_"] = { "actions.open_cwd", mode = "n" },
                     ["q"] = { "actions.close", mode = "n" },
                     ["gd"] = { "actions.cd", mode = "n" },
+                    ["gs"] = { "actions.change_sort", mode = "n" },
                     ["g."] = { "actions.toggle_hidden", mode = "n" },
                     ["+"] = { callback = oil.save, mode = "n" },
                 },
             })
-        end, 
+        end,
     }
 }
 
